1bc35de6a0bf689f4f66fc52bca9d32c8bc11076
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-02-23 15:28:53 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Login from './components/Login/Login';
 import Home from './page/Home/index';
 import Project from './page/Project/index';
 import Dashboard from './page/Dashboard/index';
-import Approve from "./page/Approve-CV/index";
-import Confirm from "./page/Confirm-CV/index";
-import Intern from "./page/Intern-List/index";
-import Group from "./page/Group-List/index";
-import Position from "./page/Position/index";
-import Tech from "./page/Tech/index";
-import Zalo from "./page/Zalo/index";
-import Settings from "./page/Settings/index";
+import Approve from './page/Approve-CV/index';
+import Confirm from './page/Confirm-CV/index';
+import Intern from './page/Intern-List/index';
+import Group from './page/Group-List/index';
+import Position from './page/Position/index';
+import Tech from './page/Tech/index';
+import Zalo from './page/Zalo/index';
+import Settings from './page/Settings/index';
 function App() {
     return (
         <Routes>
             {/* <Route path="/" element={<Login />} /> */}
             <Route path="/home" element={<Home />}>
-
                 <Route path="dashboard" element={<Dashboard />} />
                 <Route path="approve-cv" element={<Approve />} />
                 <Route path="confirm-cv" element={<Confirm />} />
@@ -31,23 +30,5 @@ function App() {
                 <Route path="settings" element={<Settings />} />
             </Route>
         </Routes>
-
-import Login from './pages/Login/Login';
-import AdminLogin from './components/AdminLogin/AdminLogin';
-import SchoolLogin from './components/SchoolLogin/SchoolLogin';
-
-
-function App() {
-    return (
-        <>
-            <Routes>
-                <Route path='/' element={<Login />} >
-                    <Route index element={<AdminLogin/>}/>
-                    <Route path='school-login' element={<SchoolLogin/>}/>
-                </Route>
-            </Routes>
-        </>
     );
 }
-
-export default App;
