d200f369718cf571739a361737e3a0da919a9429
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-02-23 15:40:50 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-
-import Login from './components/Login/Login';
 import Home from './page/Home/index';
 import Project from './page/Project/index';
 import Dashboard from './page/Dashboard/index';
@@ -13,10 +11,16 @@ import Position from './page/Position/index';
 import Tech from './page/Tech/index';
 import Zalo from './page/Zalo/index';
 import Settings from './page/Settings/index';
+import Login from './pages/Login/Login';
+import AdminLogin from './components/AdminLogin/AdminLogin';
+import SchoolLogin from './components/SchoolLogin/SchoolLogin';
 function App() {
     return (
         <Routes>
-            {/* <Route path="/" element={<Login />} /> */}
+            <Route path="/" element={<Login />}>
+               <Route index element= {<AdminLogin/>}/> 
+               <Route path='school-login' element= {<SchoolLogin />}/> 
+            </Route>
             <Route path="/home" element={<Home />}>
                 <Route path="dashboard" element={<Dashboard />} />
                 <Route path="approve-cv" element={<Approve />} />
@@ -32,3 +36,4 @@ function App() {
         </Routes>
     );
 }
+export default App
