67380a120a929db650aa082fafbba2bf85ba6cbf
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-03-01 17:51:26 +0700

    fix login admin

--- src/components/AdminLogin/AdminLogin.jsx
+++ src/components/AdminLogin/AdminLogin.jsx
@@ -1,65 +1,100 @@
 import React from 'react';
-import { Button, Input, Tooltip } from 'antd';
-import { CloseCircleOutlined } from '@ant-design/icons';
-import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
-import { Checkbox } from 'antd';
+import { Alert, Button, Form, Input , Checkbox  } from 'antd';
 import { Link } from 'react-router-dom';
 
 function AdminLogin() {
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
-        <div>
-            <div style={{ width: '22.5rem', marginLeft: '16.375rem' }}>
-                <h1 style={{color: "#4889E9"}}>Admin Login</h1>
-                <p style={{color:'#667085'}}>Please fill your detail to access your account.</p>
-                <div>
-                    <label>Email</label>
-                    <Input
-                        suffix={
-                            <Tooltip title="Extra information">
-                                <CloseCircleOutlined
-                                    style={{
-                                        color: 'rgba(0,0,0,.45)',
-                                    }}
-                                />
-                            </Tooltip>
-                        }
-                    />
-                    <label>Password</label>
-                    <Input.Password
-                        placeholder="input password"
-                        iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-                    />
-                </div>
+        <div  style={{marginLeft: "16.375rem"}}>
+            <div>
+                <h1 style={{ color: '#4889E9' }}>Admin Login</h1>
+                <p style={{ color: '#667085' }}>Please fill your detail to access your account.</p>
             </div>
-            <div
+            <Form
+                name="trigger"
                 style={{
-                    width: '22.5rem',
-                    marginLeft: '16.375rem',
-                    display: 'flex',
-                    justifyContent: 'space-between',
+                    maxWidth: 300,
                 }}
+                layout="vertical"
+                autoComplete="off"
+                onFinish={onFinish}
             >
-                <div>
-                    <Checkbox />
-                    <p style={{ display: 'inline-block' }}>Remember me</p>
-                </div>
-                <div>
-                    <a  style={{ display:"inline-block", marginTop:"0.75rem" ,color: '#DB0D4B' }}>Forgot password?</a>
-                </div>
-            </div>
-            <div style={{ width: '22.5rem', marginLeft: '16.375rem' }}>
-                <Button style={{width: "100%", backgroundColor:"#4889E9"}} type="primary">
-                    Sign in
-                </Button>
-                <Button style={{width: "100%", backgroundColor:"#E2E8F0", border:"none", marginTop:"0.5rem"}} type="primary" ghost>
-                    <Link to='/register-admin' style={{width:'100%'}}>Register</Link>
-                </Button>
-                <p style={{textAlign:"center", textTransform:"upperCase"}}>Or Login with</p>
-                <Button style={{width: "100%", color:'#333333',border:'none', boxShadow: '0 0.25rem 0.125rem -0.125rem #E2E8F0', display:"flex", justifyContent:"center", textAlign:"center"}} type="primary" ghost>
-                   <img src='src\assets\img\Logo\flat-color-icons_google.png'/>
-                    Google
-                </Button>
-            </div>
+
+                <Form.Item
+                    label="Email"
+                    name="email"                          
+                >
+                    <Input  placeholder="youremail@example.com" />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="passowrd"
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    Col={{
+                        offset: 8,
+                        span: 24,
+                    }}
+                    >
+                    <Button type="primary" style={{width:"100%", backgroundColor:"#4889E9"}} htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+                <Form.Item
+                    Col={{
+                        offset: 8,
+                        span: 24,
+                    }}
+                    >
+                    <Button style={{width:"100%", backgroundColor:"##EFF4FB"}} htmlType="submit">
+                        Sign up
+                    </Button>
+                </Form.Item>
+                <Form.Item
+                    Col={{
+                        offset: 8,
+                        span: 24,
+                    }}
+                    style={{textAlign:"center"}}
+                >
+                        OR LOGIN WITH
+                </Form.Item>
+                <Form.Item
+                    Col={{
+                        offset: 8,
+                        span: 24,
+                    }}
+                    style={{
+                        textAlign:"center", 
+                        backgroundColor:"#FFFFFF", 
+                        boxShadow: "0 5px 15px rgba(0, 0, 0, 0.3)" ,
+                        cursor: "pointer", 
+                        display: 'flex',
+                        justifyContent: 'center', /* căn giữa theo chiều ngang */
+                        alignItems: 'center', /* căn giữa theo chiều dọc */
+                        height: '32px', /* chiều cao của container */ 
+                        borderRadius: '5px',
+                        padding: '5px'
+                    }}
+                >
+                    <div style={{display:"flex", justifyContent:"center", gap:'2px', textAlign:'center'}}>
+                        <img src="src\assets\img\Logo\flat-color-icons_google.png" style={{width:"1.5rem",height:"1.5rem", marginTop:"12px"}}/>
+                        <p>Google</p>
+                    </div>                  
+                </Form.Item>
+            </Form>
         </div>
     );
 }
