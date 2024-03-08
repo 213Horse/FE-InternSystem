4a4d5765ffd2518676126124db9fe1571f4cd2b1
kanatsumi2003 <naduccva@gmail.com>
2024-03-08 10:58:44 +0700

    add listen port in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], // You may need to replace `react()` with the actual plugin you are using, like `vite-plugin-react`
+ 
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  }
+
+
+});
