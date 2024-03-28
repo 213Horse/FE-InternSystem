598e590cdaea9f0456c16f53e8fcb669fd0d3073
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-03-28 09:45:27 +0700

    update code

---
+++ src/components/Password/ResetPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ResetPassword() {
+  return (
+    <div style={{ marginLeft: '16.375rem' }}>
+      ResetPassword
+    </div>
+  )
+}
+
+export default ResetPassword
